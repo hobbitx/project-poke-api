ebae5b2c0e5f9a744f1bd4b32b73f9fb6f09de77
Robert Cristiam <robert.cristian@live.com>
2021-09-30 09:52:36 -0300

    add response for pokemons

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from src.logic.functions import requests
-pokemon = requests.get_pokemon("butterfree")
-print(pokemon)
+import json
+
+f = open('inputs/example1.json')
+data = json.load(f)
+response = {}
+for pokemon in data['pokemons']:
+    response[pokemon] = requests.get_pokemon(pokemon).to_response()
+
+print(json.dumps(response, indent=4, sort_keys=True))
